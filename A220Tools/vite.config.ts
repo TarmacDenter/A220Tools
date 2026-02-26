4dd22a07f945b2953bb3491e43e7e14b5ff0f623
michaeldempsey <michaelstdempsey@gmail.com>
2026-02-26 11:25:39 -0500

    deploy

--- A220Tools/vite.config.ts
+++ A220Tools/vite.config.ts
@@ -1,27 +1,25 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   server: {
     proxy: {
-      '/api/avwx': {
-        target: 'https://aviationweather.gov',
+      "/api/avwx": {
+        target: "https://aviationweather.gov",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/avwx/, '/api/data'),
+        rewrite: (path) => path.replace(/^\/api\/avwx/, "/api/data"),
       },
     },
   },
-})
+  base: "/A220Tools/",
+});
